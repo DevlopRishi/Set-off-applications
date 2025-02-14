3b36e554fe186b1a5b9c567702fb349f00442b35
Imtitled <134732678+DevlopRishi@users.noreply.github.com>
2025-02-14 18:29:59 +0530

    Update

---
+++ guess-name/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to the Name Guessing App!")
+
+    # Guess the user's name
+    guessed_name = input("Enter your name: ")
+    print(f"My guess for your name is {guessed_name}.")
+
+if __name__ == "__main__":
+    main()
